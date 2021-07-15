1900e915e80e10da6c7078946539d26c19d50faf
FaheedMuneer <faheedmuneer008@msitprogram.net>
2021-07-15 19:35:35 +0530

    done caeser

--- 03-applycaesarcipher-Python/applycaesarcipher.py
+++ 03-applycaesarcipher-Python/applycaesarcipher.py
@@ -11,7 +11,19 @@
 
 
 def fun_applycaesarcipher(msg, shift):
-	return ""
+	res = ''
+	for i in range(len(msg)):
+		ab = msg[i]
+		if (ab.isspace()):
+			res += ' '
+		elif (ab.isupper()):
+			res += chr((ord(ab) + shift-65) % 26 + 65)
+		else:
+			res += chr((ord(ab) + shift - 97) % 26 + 97)
+	return res
+	
+
+
 
 
 
