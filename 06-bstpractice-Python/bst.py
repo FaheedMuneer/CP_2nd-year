16c466a968f24e4efde8bdd42dd70bbc91825a03
FaheedMuneer <faheedmuneer008@msitprogram.net>
2021-08-02 09:24:51 +0530

    done BSTPrac

--- 06-bstpractice-Python/bst.py
+++ 06-bstpractice-Python/bst.py
@@ -10,13 +10,37 @@ class BST(object):
 
     def insert(self, new_val):
         # Your code goes here
-        pass
+        new = Node(new_val)
+        if (self.root == None):
+            self.root = new
+        else:
+            curr = self.root
+            pare = self.root
+            while (curr != None):
+                pare = curr
+                if (new_val < curr.value):
+                    curr = curr.left
+
+                else:
+                    curr = curr.right
+            if (new_val < pare.value):
+                pare.left = new
+            else:
+                pare.right = new
 
     def printSelf(self):
         # Your code goes here
-        pass
+        print(self.root)
         
     def search(self, find_val):
         # Your code goes here
-        pass
+        while self.root != None:
+            if self.root.value == find_val:
+                return True
+            if self.root.value < find_val:
+                self.root = self.root.right
+            else:
+                self.root = self.root.left
+        return False
+
 
